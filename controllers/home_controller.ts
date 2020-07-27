b39e8a64b06eebbba96ca226a1bcf5478f5203aa
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2020-07-27 16:22:44 +0900

    [add]monthly_controllerの追加

--- controllers/home_controller.ts
+++ controllers/home_controller.ts
@@ -4,20 +4,15 @@ import db from '../models/index';
 class HomeController {
 
   // クエリ関数
-  public static async workAll(res: express.Response) {
-    const works = await db.Work.findAll().catch((err: Error) => console.error(err));
-    res.json(works);
-  }
-
   public static async worksByCreatedAtDesc(res: express.Response) {
-    const works = await db.Work.findAll({ order: [['createdAt', 'DESC']] }).catch((err: Error) => console.error(err));
+    const works = await db.Work.findAll({ order: [['createdAt', 'DESC']], include: db.Category })
+      .catch((err: Error) => console.error(err));
     res.json(works);
   }
 
   // コントローラーアクション
   public static index(req: express.Request, res: express.Response) {
     HomeController.worksByCreatedAtDesc(res);
-    console.log('OK!')
   };
 };
 
--- controllers/monthly_controller.ts
+++ controllers/monthly_controller.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import { Op } from 'sequelize';
-import { startOfMonth, lastDayOfMonth } from 'date-fns';
+import { startOfMonth, lastDayOfMonth, addDays } from 'date-fns';
 import db from '../models/index';
 
 class MonthlyController {
 
   // クエリ関数
   public static async hoursPerCategory(req: express.Request, res: express.Response) {
-
     let startDate: any = req.query.month;
     // 前月末日に変換(DB問い合わせの条件指定で一日繰り上げになる為)
     startDate = startOfMonth(new Date(startDate));
@@ -15,18 +14,40 @@ class MonthlyController {
     // 当月末日に変換
     endDate = lastDayOfMonth(new Date(endDate));
     console.log(startDate, endDate);
-
-    const hours: Array<any/*{ [id: number]: number }*/> = await [1, 2, 3, 4].map((id: number) => {
-      db.Work.findAll({ where: { done_date: { [Op.between]: [startDate, endDate] } } })
-        //.sum('done_hours', /*{ where: { CategoryId: id } }*/)
-        .then((result: number) => {
-          //const hours_per_category: number = result;
-          //return { id: hours_per_category }
-          return 'hello';
+  
+    const hours_per_category: Array<object> = [];
+    const categories = await db.Category.count()
+      .catch((err: Error) => console.error(err));
+    const category_names = JSON.stringify(await db.Category.findAll({ attributes: ['name'] })
+      .catch((err: Error) => console.error(err)));
+    const category_names_ary = JSON.parse(category_names);
+    const total_hours = await db.Work.sum('done_hours', { where: { done_date: { [Op.between]: [startDate, endDate] } } })
+      .catch((err: Error) => console.error(err));
+    
+    let category_id = 1;
+    for (let i = 1; i <= categories; i++) {
+      await db.Work.sum('done_hours', { where: { done_date: { [Op.between]: [startDate, endDate] }, CategoryId: i } })
+        .then((category_sum: number) => {
+          const category_name = category_names_ary[category_id - 1].name;
+          const category_rate = Math.round((category_sum / total_hours) * 100);
+          hours_per_category.push({
+            'id': category_id,
+            'category_name': category_name,
+            'hours': category_sum,
+            'rate': category_rate
+          });
         })
-        .catch ((err: Error) => console.error(err));
-    })
-    res.json(hours);
+        .catch((err: Error) => console.error(err));
+      category_id++;  
+    }
+    
+    hours_per_category.push({
+      'id': category_id,
+      'category_name': '合計',
+      'hours': total_hours,
+      'rate': 100
+    });
+    res.json(hours_per_category);
   }
   
 
